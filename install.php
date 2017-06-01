45df514091367095cb492b444a58d23029287939
Pone Z Pyo <phonezawphyo@gmail.com>
2017-06-01 05:37:26 +0000

    [add]indexes on additional columns

--- install.php
+++ install.php
@@ -8,4 +8,9 @@ $altertable->execute();
 $altertable2 = $db->pdo->prepare('ALTER TABLE fort_sightings ADD last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP');
 $altertable2->execute();
 
+$createindex = $db->pdo->prepare('CREATE INDEX last_updated_index ON sightings (last_updated)');
+$createindex->execute();
+
+$createindex2 = $db->pdo->prepare('CREATE INDEX last_updated_index ON fort_sightings (last_updated)');
+$createindex2->execute();
 echo "Successfully applied any changes that were needed!";
