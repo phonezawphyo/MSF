e511b73fcbf0930ad12f68e348a148ff8d6e55e8
Pone Z Pyo <phonezawphyo@gmail.com>
2017-06-05 01:29:44 +0000

    [fix]cleanup.php

--- cleanup.php
+++ cleanup.php
@@ -1,10 +1,24 @@
 <?php
 include('config.php');
 
-$cleanup = $db->pdo->prepare('DELETE sightings WHERE last_updated BETWEEN DATE_SUB(NOW(),INTERVAL 7 DAY) AND DATE_SUB(NOW(),INTERVAL 1 DAY)');
-$cleanup->execute();
+$timestamp = strtotime("-1 day");
+$timestamp_str = gmdate('Y-m-d H:i:s', $timestamp);
 
-$cleanup = $db->pdo->prepare('DELETE fort_sightings WHERE last_updated BETWEEN DATE_SUB(NOW(),INTERVAL 7 DAY) AND DATE_SUB(NOW(),INTERVAL 1 DAY)');
-$cleanup->execute();
+echo "GMT timestamp: ". $timestamp . "\n";
+echo "GMT datetime: " . $timestamp_str . "\n";
+
+#$cleanup = $db->pdo->prepare('DELETE FROM sightings WHERE last_updated BETWEEN DATE_SUB(NOW(),INTERVAL 7 DAY) AND DATE_SUB(NOW(),INTERVAL 1 DAY)');
+#$cleanup->execute();
+$cleanup = $db->pdo->prepare('DELETE FROM sightings WHERE last_updated < ?');
+$cleanup->execute(array($timestamp_str));
+echo "Cleaning sightings. Deleted rows:" . $cleanup->rowCount() . "\n";
+
+$cleanup = $db->pdo->prepare('DELETE FROM mystery_sightings WHERE first_seen < ?');
+$cleanup->execute(array($timestamp));
+echo "Cleaning mystery_sightings. Deleted rows:" . $cleanup->rowCount() . "\n";
+
+$cleanup = $db->pdo->prepare('DELETE FROM fort_sightings WHERE last_updated < ?');
+$cleanup->execute(array($timestamp_str));
+echo "Cleaning fort_sightings. Deleted rows:" . $cleanup->rowCount() . "\n";
 
 echo "Successfully executed cleanup at: " . date('Y-m-d H:i:s') . "\n";
